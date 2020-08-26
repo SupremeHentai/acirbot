ac44200d809f5087f8d9efc945c3e9e6cd9cf621
Conrad <Test@Conrads-MacBook-Pro.local>
2020-08-26 21:05:14 +1200

    cmd update

--- bot.js
+++ bot.js
@@ -2,15 +2,15 @@ const Discord = require('discord.js');
 var fs = require('fs');
 const client = new Discord.Client();
 const prefix = "73:";
-
-var token = JSON.parse(fs.readFileSync("auth.json")).token;
+ 
+var token = "NzM2ODcyNTUzOTM4OTQ0MDcw.Xx1ICw.EEDRvCRShMHrlqwbUSLBWe8gO6k";
 var basedChannels = ["cable-street-bets", "alphatism"];
-
-
+ 
+ 
 client.once('ready', () => {
 	console.log('Ready!');
 });
-
+ 
 client.on('message', message => {
     if (message.content.includes("73") && message.member.user.id != "736872553938944070") {
         console.log(message.member.user.id);
@@ -18,28 +18,28 @@ client.on('message', message => {
 	    //message.channel.send('<:osuhow:713918429073113102>');
     }
 });
-
+ 
 client.on('message', message => {
     //console.log(message.content.slice(0,8));
     if (message.content.slice(0,7).toLowerCase() == prefix + " say") {
         message.channel.send(message.content.slice(8,message.content.length));
     }
 });
-
+ 
 client.on('message', message => {
     if (message.content == "FEAR" || message.content == "FEAR!") {
         message.channel.send("FEAR GOD AND GIVE GLORY TO HIM!")
     }
 });
-
+ 
 client.on('message', message => {
     if (message.content.toLowerCase() == "guacamole") {
         message.channel.send("🥑👨🏿🍆");
     }
 });
-
+ 
 client.on('message', message => {
-    console.log(message.channel.name);
+    if (message.guild.name == "UoA: level 3 confessions") return;
     len = message.content.length;
     if (message.content.slice(len-3,len).toLowerCase() == "nig") {
         if (basedChannels.includes(message.channel.name)) {
@@ -48,7 +48,7 @@ client.on('message', message => {
         }
     }
 });
-
+ 
 client.on('message', message => {
     len = message.content.length;
     if (message.content.includes("hentai")) {
@@ -58,28 +58,17 @@ client.on('message', message => {
         }
     }
 });
-
+ 
 client.on('message', message => {
     if (message.content.toLowerCase() == "no") {
         message.channel.send("cturne");
     }
 });
-
+ 
 client.on('message', message => {
+    if (message.channel.name == "👶part1") return;
+    if (message.author.bot) return;
     if (message.member.user.id == "736872553938944070") return;
-    /*console.log(message.content.toLowerCase().slice(0,2))
-    if (message.content.toLowerCase().slice(0,2) == "my") {
-        var newStr = "OUR " + message.content.slice(3,message.content.length) + " comrade";
-        message.channel.send(newStr);
-    } else {
-        var str = message.content
-        for (var i = 0; i < str.length; i++) {
-            if (str.charAt(i).toLowerCase() == "m" && str.charAt(i+1).toLowerCase() == "y" && str.charAt(i+2).toLowerCase() === " ") {
-                var newStr = "OUR " + message.content.slice(i+3,message.content.length) + " comrade";
-                message.channel.send(newStr);
-            }
-        }
-    }*/
     var string = message.content;
     if (string.toLowerCase().includes("my ")) {
         const idx = string.indexOf("my");
@@ -89,6 +78,40 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
+    if (message.author.bot) return;
+    if (message.channel.name == "👶part1") return;
+    if (message.member.user.id == "736872553938944070") return;
+    var string = message.content;
+    if (string.toLowerCase().includes("i ")) {
+        const idx = string.indexOf("i ");
+        const item = string.slice(idx + 2);
+        message.channel.send("WE " + item + " comrade");
+    }
+});
+
+client.on('message', message => {
+    if (message.member.user.id == "736872553938944070") return;
+    var string = message.content;
+    if (string.toLowerCase().includes("pain")) {
+        message.channel.send("🥖");
+    }
+});
+
+client.on('message', message => {
+    console.log(message.channel.name);
+    if (message.channel.name == "👶part1") return;
+    if (message.author.bot) return;
+    if (message.member.user.id == "736872553938944070") return;
+    var string = message.content;
+    if (string.toLowerCase().includes("i'm ")) {
+        const idx = string.indexOf("i'm");
+        const item = string.slice(idx + 4);
+        message.channel.send("WE'RE " + item + " comrade");
+    }
+});
+ 
+client.on('message', message => {
+    if (message.guild.name == "UoA: level 3 confessions") return;
     if (message.content.toLowerCase() == "around the world") {
         /*for (var i = 0; i < 12; i++) {
             message.channel.send("around the world\naround the world\naround the world\naround the world\n\naround the world\naround the world\naround the world\naround the world\n\naround the world\naround the world\naround the world\naround the world\n\n");
@@ -98,22 +121,22 @@ client.on('message', message => {
 Around the world, around the world 
 Around the world, around the world 
 Around the world, around the world
-            
+ 
 Around the world, around the world 
 Around the world, around the world 
 Around the world, around the world 
 Around the world, around the world
-            
+ 
 Around the world, around the world 
 Around the world, around the world 
 Around the world, around the world 
 Around the world, around the world
-            
+ 
 Around the world, around the world 
 Around the world, around the world 
 Around the world, around the world 
 Around the world, around the world
-            
+ 
 Around the world, around the world 
 Around the world, around the world 
 Around the world, around the world 
@@ -124,8 +147,9 @@ Around the world, around the world
         message.channel.send(attachment);*/
     }
 });
-
+ 
 client.on('message', message => {
+    if (message.guild.name == "UoA: level 3 confessions") return;
     if (message.content.toLowerCase() == "ram ranch") {
         message.channel.send(`
 18 naked cowboys in the showers at Ram Ranch!
@@ -148,15 +172,16 @@ Cowboys love big hard throbbing cocks!
         }
     }
 );
-
+ 
 /*
 client.on('message', message => {
     if (message.content.slice(0,8).toLowerCase() == prefix + " eval") {
         message.channel.send(eval(message.content.slice(9,message.content.length)))
     }
 });*/
-
+ 
 client.on('message', message => {
+    if (message.guild.name == "UoA: level 3 confessions") return;
     rand = Math.random()
     console.log(rand);
     if (rand > 0.99) {
@@ -164,5 +189,12 @@ client.on('message', message => {
         message.channel.send(attachment);
     }
 });
-
+ 
+client.on('message', message => {
+    if (message.content == "+gloria") {
+        const attachment = new Discord.MessageAttachment('gloria.png');
+        message.channel.send(attachment);
+    }
+});
+ 
 client.login(token);
